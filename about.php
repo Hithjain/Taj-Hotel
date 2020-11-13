698b2338a3d127a85b1a36196475a925f765784b
Hithjain <34999541+Hithjain@users.noreply.github.com>
2020-11-13 20:41:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ABOUT TAJ</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="css/style.css"rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body style="margin-top:50px;">
+<?php
+      include('Menu Bar.php')
+  ?><br>
+<div class="container-fluid text-center">
+<div class="container">
+  <div class="row content">
+    <div class="col-sm-12">
+      <h1 id="font">{ TAJ GROUP OF HOTELS }</h1><br>
+      <p class="text-justify">The Taj Mahal Palace opened in Mumbai, then Bombay, in 1903, giving birth to the country’s first harbour landmark. The recently trademarked flagship hotel overlooks the majestic Gateway of India. This legendary luxury hotel has played host to kings, dignitaries and eminent personalities from across the globe, and is acknowledged as a world leader in hospitality. Each of the rooms 285 rooms & suites are a striking blend of nostalgic elegance, rich history and modern facilities. Strategically located in Mumbai’s prime historical and commercial hub, The Taj Mahal Palace is walking distance from Colaba Causeway, known for its touristy tiny roadside shops. The city’s financial hub, parliament, stadium, offices and shopping districts are all located within close proximity; as is a flourishing art and heritage precinct with must visit museums, art galleries, churches and synagogues. The airports are an hour away. For those who would like to experience fine dining in style, we host 9 of the city’s most acclaimed restaurants and bars, renowned for delectable cuisines from India, Europe, the Middle East and the Orient. Relish authentic Indian cuisine at Masala Kraft; world famous Japanese dishes at Wasabi by Morimoto; exquisite Eastern Mediterranean food at Souk and Chinese at Golden Dragon. Dine all day at Shamiana and choose from the exotic range of sumptuous desserts at La Patisserie. Unwind with a refreshing meal at Aquarius, the poolside restaurant; and visit the stylish Harbour Bar. Signature experiences such as the afternoon tea at Sea Lounge and private dining at the Chef’s Studio make your stay with us truly memorable.Mumbai’s original luxury hotel has 11 newly refurbished halls and venues, ideal for occasions ranging from formal meetings to elaborate weddings. The Ballroom, spacious enough for 450 guests, is perfect for conferences and grand private events.You could opt for a private dinner or yacht party if you are looking to entertain in true style. Our globally acclaimed Golden Keys Concierge would be happy to help, and caters to all your requirements in the city. Come, experience the legend.</p>
+<h1 align="center"id="font">[ Services ]</h1><br>
+<p class="text-justify">Taj provides a wide array of guest services and on-site facilities. Amenities include: on-site food and beverage (room service and restaurants), meeting and conference services and facilities, fitness center, and business center. Full-service hotels range in quality from mid-scale to luxury. This classification is based upon the quality of facilities and amenities offered by the hotel. [8] </p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Pmaia6zSyCY?start=2" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div><br><br>
+  <div class="row"align="center">
+  <!--map Start Here-->
+  <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+      <a href="#" type="button" data-toggle="modal" data-target="#myModal"><img src="image/icon/icon-01.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+  </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+        <iframe class="frame"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.9012415990155!2d77.37085911440647!3d28.602739392180986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce57ca5ff4435%3A0x6de631b7a5bfdacb!2sUnion+Bank+Of+India!5e0!3m2!1sen!2sin!4v1555502551059!5m2!1sen!2sin" width="550"height="300" frameborder="0" style="border:1" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Map Close Here-->
+
+        <div class="col-sm-2">
+          <h4><b>Address</b></h4>
+          <b>Chhatrapati Shivaji International Airport, T1, Off, Western Express Hwy, Navpada, Vile Parle East, Santacruz East, Mumbai, Maharashtra 400099</b>
+        </div>
+        <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+            <a href="#"><img src="image/icon/icon-02.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+        </div>
+         <div class="col-sm-2">
+          <h4><b>Phone</b></h4>
+              <b>(+91) 8108812900</b>
+        </div>
+        <div class="col-sm-2"style="background-color:#27303b;height:80px;width:100px;">
+            <a href="#"><img src="image/icon/icon-03.png"width="60px;"height="50px;"style="margin-top:15px;"></a>
+        </div>
+         <div class="col-sm-2">
+          <h4><b>Email-Id</b></h4>
+              <b>taj@gmail.com</b>
+        </div>
+  </div><br><br>
+</div>
+</div>
+<?php
+  include('Footer.php')
+?>
+</body>
+</html>
---
+++ indexrest.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+      include('Menu Bar.php')
+  ?><br>
+
+	<meta charset="utf-8">
+	<title>Zentro</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<link rel="stylesheet" href="css/bootstrap.minrest.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/nivo-lightbox.css">
+	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+	<link rel="stylesheet" href="css/stylerest.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+<!-- preloader section -->
+<section class="preloader">
+	<div class="sk-spinner sk-spinner-pulse"></div>
+</section>
+
+<!-- home section -->
+<section id="home" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<h1>ZENTRO RESTAURANT</h1>
+				<h2>Taj Hotel,Mumbai</h2>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!-- gallery section -->
+<section id="gallery" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
+				<h1 class="heading">Food Gallery</h1>
+				<hr>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.3s">
+				<a href="images/gallery-img1.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img1.jpg" alt="gallery img"></a>
+				<div>
+					<h3>Lemon-Rosemary Prawn</h3>
+				</div>
+				<a href="images/grilledchicken.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/grilledchicken.jpg" alt="gallery img"></a>
+				<div>
+					<h3>Grilled Chicken</h3>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<a href="images/gallery-img3" data-lightbox-gallery="zenda-gallery"><img src="images/gallery-img3.jpg" alt="gallery img"></a>
+				<div>
+					<h3>Garlic Bread</h3>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.9s">
+				<a href="images/burger.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/burger.jpg" alt="gallery img"></a>
+				<div>
+					<h3>Portobello Mushroom burgers</h3>
+				</div>
+				<a href="images/pizza.jpg" data-lightbox-gallery="zenda-gallery"><img src="images/pizza.jpg" alt="gallery img"></a>
+				<div>
+					<h3>Classic Italian Pizza</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!-- menu section -->
+<section id="menu" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
+				<h1 class="heading">Special Menu</h1>
+				<hr>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Grilled Butter Chicken ................ <span>Rs.345</span></h4>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Tandoori Paneer Tikka ........................... <span>Rs.300</span></h4>
+
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Mysore Masla Dosa ........................ <span>Rs.145</span></h4>
+
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Onion Uthappam .......................... <span>Rs.165</span></h4>
+
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Double Patty Burger(Chicken/Paneer) ...................... <span>Rs.450/550</span></h4>
+
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Alfredo Pasta(Veg/Chicken)........................ <span>Rs.350/395</span></h4>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Apple Soaked Mojito................... <span>Rs.375</span></h4>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h4>Long Island Iced Tea ..................... <span>Rs.320</span></h4>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!-- team section -->
+<section id="team" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
+				<h1 class="heading">Meet Zentro chefs</h1>
+				<hr>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.3s">
+				<img src="images/gordon.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Godron Ramsay</h4>
+				<h3>Main Chef</h3>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<img src="images/thomas.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Thomas Keller</h4>
+				<h3>Pizza Specialist</h3>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.9s">
+				<img src="images/alian.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Alain Ducasse</h4>
+				<h3>New Baker</h3>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- footer section -->
+<footer class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<h2 class="heading">Contact Info.</h2>
+				<div class="ph">
+					<p><i class="fa fa-phone"></i> Phone</p>
+					<h4>090-080-0760</h4>
+				</div>
+				<div class="address">
+					<p><i class="fa fa-map-marker"></i> Our Location</p>
+					<h4>120 Taj Hotel,Mumbai</h4>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<h2 class="heading">Open Hours</h2>
+					<p>Mon-Fri <span>10:30 AM - 10:00 PM</span></p>
+					<p>Saturday <span>9:00 AM - 8:00 PM</span></p>
+					<p>Sunday <span>11:30 AM - 10:00 PM</span></p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+<!-- copyright section -->
+<section id="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<h3>ZENTRO</h3>
+				<p>Copyright © Zentro Restaurant and Cafe
+
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- JAVASCRIPT JS FILES -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.minrest.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/nivo-lightbox.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
